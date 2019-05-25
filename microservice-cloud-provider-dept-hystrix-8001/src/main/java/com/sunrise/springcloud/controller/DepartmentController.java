397630457d10d33d36bcd23f09c927a9495503bc
lzhaoyang <575563079@qq.com>
2019-05-25 18:34:20 +0800

    服务熔断 hystrix

---
+++ microservice-cloud-provider-dept-hystrix-8001/src/main/java/com/sunrise/springcloud/controller/DepartmentController.java
@@ -0,0 +1,69 @@
+package com.sunrise.springcloud.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.sunrise.springcloud.entities.Department;
+import com.sunrise.springcloud.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @date: 2019/5/24 20:28
+ * @auther: sunrise
+ * @Gmail: liu575563079@gmail.com
+ */
+@RestController
+public class DepartmentController {
+    @Autowired
+    private DepartmentService departmentService;
+
+    //服务发现
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+    public boolean add(@RequestBody Department department) {
+        return departmentService.add(department);
+    }
+
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "hystrixFallBackProcessGet")  //该方法调用失败后会自动启动hystrixFallBackProcessGet这个方法
+    public Department get(@PathVariable("id") Long id) {
+        Department department = departmentService.get(id);
+        if (null == department) {
+            throw new RuntimeException("id: " + id + "不存在记录");
+        }
+        return department;
+    }
+
+    public Department hystrixFallBackProcessGet(@PathVariable("id") Long id) {
+        return new Department()
+                .setDeptNo(id)
+                .setDeptName("id: " + id + "该id无记录，不在数据库").setDbSource("@hystrixFallBackProcessGet，请及时检查服务");
+    }
+
+    @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+    public List<Department> list() {
+        return departmentService.list();
+    }
+
+
+    //服务发现
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = discoveryClient.getInstances("MICROSERVICE-CLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.discoveryClient;
+    }
+
+}
