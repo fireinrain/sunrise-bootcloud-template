67124217994fe92e57ec66cb6a9d3e81ef5d310c
lzhaoyang <575563079@qq.com>
2019-05-25 14:29:04 +0800

    负载均衡 ribbon 随机

--- microservice-cloud-consumer-dept-80/src/main/java/com/sunrise/springcloud/cfgbeans/ConfigBean.java
+++ microservice-cloud-consumer-dept-80/src/main/java/com/sunrise/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.sunrise.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +13,18 @@ import org.springframework.web.client.RestTemplate;
  * @auther: sunrise
  * @Gmail: liu575563079@gmail.com
  */
-@Configuration
+@Configuration   //相当于spring的applicationContext.xml
 public class ConfigBean {
     @Bean
     @LoadBalanced   //开启客户端负载均衡ribbon
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule myRandomRule(){
+        return new RandomRule();  //替换掉Ribbon默认的轮训算法
+                                    //默认为RoundRobinRule
     }
 
 
